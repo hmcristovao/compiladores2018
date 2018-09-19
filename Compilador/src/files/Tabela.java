531b4c32d33ab77fee0d51e745cdbaa7f5ad440c
marif <marif@172.31.24.203>
2018-09-19 16:01:00 -0300

    Tabela

---
+++ Compilador/src/files/Tabela.java
@@ -0,0 +1,31 @@
+package files;
+
+import java.util.*;
+
+public class Tabela {
+	
+	static HashMap<String, Simbolo> tab = new HashMap<String, Simbolo>();
+	
+	
+	public void insereSimbolo(Simbolo simbolo) {
+		tab.put(simbolo.getNome(), simbolo);
+	}
+
+	public boolean verificaSimbolo(Simbolo simbolo) {
+		return tab.containsKey(simbolo.getNome());
+	}
+
+	public int tamanhoTabela() {
+		return tab.size();
+	}
+	
+	public Simbolo pesquisaTabela(String chave) {
+		return tab.get(chave);
+	}
+	
+	public void imprimeTabela(){
+		System.out.println(tab);
+	}
+	
+	
+}
