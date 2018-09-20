084b65fd7da1ccf37183d99684c63a89e906500b
aluno <aluno@LABCOMP-009W.dcel.ceunes.ufes.br>
2018-09-20 14:25:41 -0300

    Alterações aula 20/09/2018

--- Compilador/src/Tabela.java
+++ Compilador/src/Tabela.java
@@ -2,11 +2,20 @@ import java.util.*;
 
 public class Tabela {
 	static Simbolo simb;
-	public static void addVar(HashMap<String, Simbolo> tab, Token t, Token v) {
+	private int marcador = 1; // armazena a última referência
+	
+	public HashMap<String, Simbolo> tab;
+	
+	Tabela(){
+		tab = new HashMap<String, Simbolo>();
+	}
+	
+	   // incluída na tabela
+	public void addVar(Token t, Token v) {
 		if(tab.containsKey(v.image) == false) { //caso nao contenha a chave
   	  		simb = new Simbolo();
 	  		simb.setNome(v.image); //salva o nome 
-	  		simb.setReferencia(simb.incMarcador(t.image)); //calcula a referencia do próximo e seta a minha referencia
+	  		simb.setReferencia(incMarcador(t.image)); //calcula a referencia do próximo e seta a minha referencia
 			if(t.image.equals(Tipo.NUMERO.getNome())) { simb.setTipo(Tipo.NUMERO); /*salva o tipo da variavel*/ }
 			if(t.image.equals(Tipo.PALAVRA.getNome())) { simb.setTipo(Tipo.PALAVRA); /*salva o tipo da variavel*/ }
 	  		tab.put(v.image, simb); //salva o simbolo na tabela hashing  
@@ -15,9 +24,29 @@ public class Tabela {
   	  	}
 	}
 	
-	public static void verificaDeclarada(HashMap<String, Simbolo> tab, Token v) {
+	public void verificaDeclarada(Token v) {
 		if(tab.containsKey(v.image) == false) { //caso nao contenha a chave
 			System.out.println("Erro: variável "+v.image+" não declarada na linha "+v.endLine );
   	  	}
+	}
+	
+	public int incMarcador(String tipo) {
+	   if(tipo.equals(Tipo.PALAVRA.getNome())) {
+		   marcador = marcador+1;
+		   return marcador-1;
+	   }else if(tipo.equals(Tipo.NUMERO.getNome())) {
+		   marcador = marcador+2;
+		   return marcador-2;
+	   }else {
+		   return 0; //necessario? 
+	   }
+   }
+	
+	public int getMarcador() {
+		return marcador;
+	}
+	
+	public void setMarcador(int marcador) {
+		marcador = marcador;
 	}
 }
