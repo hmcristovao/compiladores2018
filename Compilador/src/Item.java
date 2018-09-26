a1504964be055fb369dc87888d08c54be7904bf9
Eric Pereira <eric.pereira@hotmail.com>
2018-09-26 20:21:03 -0300

    Adição da classe item

---
+++ Compilador/src/Item.java
@@ -0,0 +1,35 @@
+class Item {
+
+   private Tipo tipo; // é uma enumeração que contém os tipos usados na linguagem, tais como: número, variável, operador etc.
+
+   private String descricao;
+   
+   private String valor;
+
+public Tipo getTipo() {
+	return tipo;
+}
+
+public void setTipo(Tipo tipo) {
+	this.tipo = tipo;
+}
+
+public String getDescricao() {
+	return descricao;
+}
+
+public void setDescricao(String descricao) {
+	this.descricao = descricao;
+}
+
+public String getValor() {
+	return valor;
+}
+
+public void setValor(String valor) {
+	this.valor = valor;
+}
+
+        // criar aqui os métodos de interface para os campos
+
+}
