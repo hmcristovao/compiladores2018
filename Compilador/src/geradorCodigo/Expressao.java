80ea6ddfcefca95c8425a17471959ccbbc829401
Thiago <Thiago@DESKTOP-6SI201Q.local>
2018-10-04 20:22:32 -0300

    Atualização da Classe Expressão

--- Compilador/src/geradorCodigo/Expressao.java
+++ Compilador/src/geradorCodigo/Expressao.java
@@ -3,14 +3,21 @@ import java.util.LinkedList;
 import semantico.*;
 
 public class Expressao {
-	static LinkedList<Item> listaExp = new LinkedList<Item>();
-
+	public LinkedList<Item> expressaoInfixa;
+	public LinkedList<Item> expressaoPosfixa;
+	
+	public Expressao() {
+		expressaoInfixa  = new LinkedList<Item>();
+		expressaoPosfixa  = new LinkedList<Item>();
+		
+	}
+	
 	public void add(Tipo _tipo, String _valor) {
 		Item novo= new Item(_tipo, _valor);
-		listaExp.add(novo);
+		expressaoInfixa.add(novo);
 	}
 	
 	public void imprime() {
-		System.out.println(this.listaExp);
+		System.out.println(this.expressaoInfixa);
 	}
 }
